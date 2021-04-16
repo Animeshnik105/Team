6fde7ab1f0751879a2d131e9f7bdff980c3e59a0
Sp1kEMy <62740671+Sp1kEMy@users.noreply.github.com>
2021-04-16 09:45:04 +0700

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Security.Principal;
+
+namespace CreateAuthorize
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (AuthorizationWindow.dep == 2)
+            {
+                BD.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void KB1(object sender, RoutedEventArgs e)
+        {
+            //Room1Window room1window = new Room1Window();
+            //room1window.Show();
+            //this.Close();
+        }
+
+        private void KB2(object sender, RoutedEventArgs e)
+        {
+            //Room2Window room2window = new Room2Window();
+            //room2window.Show();
+            //this.Close();
+        }
+
+        private void KB3(object sender, RoutedEventArgs e)
+        {
+            //Room3Window room3window = new Room3Window();
+            //room3window.Show();
+            //this.Close();
+        }
+
+        private void BD1(object sender, RoutedEventArgs e)
+        {
+            DataBase database = new DataBase();
+            database.Show();
+            this.Close();
+        }
+
+        private void CHANGE(object sender, RoutedEventArgs e)
+        {
+            AuthorizationWindow authorizationWindow = new AuthorizationWindow();
+            authorizationWindow.Show();
+            this.Close();
+        }
+    }
+ }
